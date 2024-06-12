d03b0ce5c6b179a8ae0b22a25a2c683a7981b584
luisf <vboxuser@Ubuntu22.myguest.virtualbox.org>
2024-06-12 11:06:15 -0300

    construindo o cabecalho

---
+++ src/styles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle, styled } from 'styled-components'
+
+export const cores = {
+  branca: '#eeeeee',
+  preta: '#111111',
+  cinza: '#333333',
+  verde: '#10ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+  body {
+    background-color: ${cores.preta};
+    color: ${cores.branca};
+    padding-top: 40px;
+  }
+`
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
