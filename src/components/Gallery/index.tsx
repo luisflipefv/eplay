d96366d698fa2873b2a486aa77a9f5a3d05f9a90
luisf <vboxuser@Ubuntu22.myguest.virtualbox.org>
2024-06-27 10:50:45 -0300

    :lipstick: feat: Adicionando galeria

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,123 @@
+import play from '../../assets/images/play.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+import resident from '../../assets/images/resident.png'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import zoom from '../../assets/images/zoom.png'
+import close from '../../assets/images/close.png'
+import Section from '../Section'
+import { Item, Items, Action, Modal, ModalContent } from './styles'
+import { useState } from 'react'
+
+interface GalleryItem {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'image',
+    url: spiderman
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=gbagGZEuhW3FpRHo'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    type: 'image',
+    url: '',
+    isVisible: false
+  })
+
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'video') return play
+    return zoom
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
+  return (
+    <>
+      <Section title={'Galeria'} background={'black'}>
+        <Items>
+          {mock.map((media, index) => (
+            <Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMediaCover(media)}
+                alt={`Mídia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a mídia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal className={modal.isVisible ? 'visivel' : ''}>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={close}
+              alt="Fechar"
+              onClick={() => {
+                closeModal()
+              }}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} alt="video youtube" />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => {
+            closeModal()
+          }}
+        ></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
 import Section from '../../components/Section'
+import zelda from '../../assets/images/zelda.png'
+import Gallery from '../../components/Gallery'
 
 const Product = () => {
   const { id } = useParams()
@@ -34,9 +36,7 @@ const Product = () => {
           jogo
         </p>
       </Section>
-      <Section title="Galeria" background="black">
-        <div>fotos</div>
-      </Section>
+      <Gallery name="Jogo Teste" defaultCover={zelda} />
     </>
   )
 }
